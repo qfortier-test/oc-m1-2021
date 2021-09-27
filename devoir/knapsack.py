0aba6b0a5de693a673af8ad69c0b35155041835b
qfortier-test <81088908+qfortier-test@users.noreply.github.com>
2021-09-27 21:42:39 +0200

    Update knapsack.py

--- devoir/knapsack.py
+++ devoir/knapsack.py
@@ -5,3 +5,4 @@ def knapsack(C, objects):
  
 objects = [(2, 1), (3, 7), (6, 10), (5, 10), (8, 13), (2, 1), (2, 1)] # chaque objet est un couple (poids, valeur)
 print(knapsack(10, objects)) # knapsack(10, objets) doit renvoyer 18
+g
